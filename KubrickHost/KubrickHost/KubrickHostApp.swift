cc465c874eb6f0feeb7c0f92da63631ca5dd92b5
Kevin Wooten <kevin@outfoxx.io>
2023-10-11 17:50:54 -0700

    Reorganize tests to use a host application via Xcode project
    This ensures the issues fixed in commit bf409b21d102e657fcd96972cc6022f8b5093d09  are tested as they only appear when using the pacakge from an App/Framework.

---
+++ KubrickHost/KubrickHost/KubrickHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  KubrickHostApp.swift
+//  KubrickHost
+//
+//  Created by Kevin Wooten on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct KubrickHostApp: App {
+  var body: some Scene {
+    WindowGroup {
+      EmptyView()
+    }
+  }
+}
